799f27403396abf768bac51449ea82739cf82a3f
Jorge Marcelino <johnmirra@gmail.com>
2017-12-15 22:10:57 +0000

    Merge

---
+++ modules/Account/Database/Migrations/2017_12_02_182048_create_account_user_table.php
@@ -0,0 +1,39 @@
+<?php
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('account_user', function (Blueprint $table) {
+            $table->unsignedInteger('account_id');
+            $table->unsignedInteger('user_id');
+
+            $table->timestamps();
+
+            $table->foreign('account_id')->references('id')->on('accounts')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->primary(['account_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('account_user');
+    }
+}
---
+++ modules/Account/Database/Migrations/2017_12_02_182859_add_user_to_accounts.php
@@ -0,0 +1,34 @@
+<?php
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserToAccounts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->unsignedInteger('user_id')->nullable()->after('iban');
+
+            $table->foreign('user_id')->references('id')->on('users')
+            ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('accounts', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+    }
+}
